27c20f7c326e4de4283a5a5a54b3a6f4c401ccb9
pradeep.pp <pradeep.pp@gmail.com>
2020-09-12 13:00:57 -0400

    Create Login Tests and Shopping tests. Closes #1

---
+++ src/test/java/demo/LoginTests.java
@@ -0,0 +1,48 @@
+package demo;
+
+import org.junit.After;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.*;
+
+public class LoginTests {
+    @BeforeMethod
+    public void beforeEachMethod(){
+        System.out.println("I execute before each method");
+    }
+    @AfterMethod
+    public void afterEachMethod(){
+        System.out.println("I execute after each method");
+    }
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before LoginTests class");
+    }
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After LoginTests class");
+    }
+
+    @Test(testName = "Login test with valid username and password",
+            description = "Enter valid username and invalid password" +
+                    " and click signin button",priority = 1)
+    public void loginTest(){
+        System.out.println("Login test with valid user");
+    }
+    @Test(testName = "Login test with invalid username and password",priority = 2)
+    public void loginTestInvalidUser(){
+        System.out.println("Login test with invalid user");
+    }
+
+    @Test(testName = "Verify home page title",priority = 3)
+    public void homePageTest(){
+        System.out.println("Homepage validation");
+        Assert.assertEquals("Myban","MyBank", "Page title is incorrect");
+    }
+
+    @Test(testName = "Verify logout",priority = 4)
+    public void logout(){
+        System.out.println("logout successful");
+        throw new SkipException("Skipping the test");
+    }
+}
---
+++ src/test/java/demo/ShoppingTests.java
@@ -0,0 +1,14 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class ShoppingTests {
+    @Test
+    public void productSearch(){
+        System.out.println("Searching for a product");
+    }
+    @Test
+    public void addProductToShppingCart(){
+        System.out.println("Adding a product to shopping cart");
+    }
+}
